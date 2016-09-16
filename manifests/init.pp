2cd4e16f48bbefba617c5810736ebfa3bb77c69e
Peterclaver Emedom <pemedom1_ms@rocky-pup2k15mas-01.us.umuc.edu>
2016-09-16 15:24:17 -0400

    modified manifests/init.pp file

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,14 +3,18 @@
 class pm_mcelog {
 #$mce_package = 'mcelog'
 
-if $mcelog_exists == 1 {
-  fail("Package is already absent. Unpin node from node group.")
-}
+#if $mcelog_exists == 1 {
+#  fail("Package is already absent. Unpin node from node group.")
+#}
 case $::osfamily {
   'Windows':{
     fail("Operating system not supported")
 }
   'RedHat':{
+if $mcelog_exists == 1 {
+  fail("Package is already absent. Unpin node from node group.")
+}
+
 package { 'mcelog':
   ensure => 'absent',
 
