6952214a8c9275f73835c4400197c05ff2afbffe
Peterclaver Emedom <pemedom1_ms@rocky-pup2k15mas-01.us.umuc.edu>
2016-09-16 15:21:35 -0400

    modified manifests/init.pp file

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 ## The aim of this modules is to remove mcelog package in all Rhel servers.
 
 class pm_mcelog {
-$mce_package = 'mcelog'
+#$mce_package = 'mcelog'
 
 if $mcelog_exists == 1 {
   fail("Package is already absent. Unpin node from node group.")
