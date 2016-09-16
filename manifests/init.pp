4c2204aabb1b09ce54e65afbc5a9634178120987
Peterclaver Emedom <pemedom1_ms@rocky-pup2k15mas-01.us.umuc.edu>
2016-09-16 14:35:33 -0400

    modified manifests/init.pp file

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@
 class pm_mcelog {
 $mce_package = 'mcelog'
 
-if $mce_package == 'absent' {
+if $mce_package == 'absent': {
   warning("Package is already absent. Unpin node from node group.")
 }else {
 
