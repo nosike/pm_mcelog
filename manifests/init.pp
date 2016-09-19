6783410bbd23006a053d255f3ffce8aec4ec5bdb
Peterclaver Emedom <pemedom1_ms@rocky-pup2k15mas-01.us.umuc.edu>
2016-09-18 22:08:48 -0400

    Modified manifests/init.pp file

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,18 @@
 ## The aim of this modules is to remove mcelog package in all Rhel servers.
 
 class pm_mcelog {
-$mcelog_present = mcelog_exists("/usr/sbin/mcelog")
-  notify{ "Check File = $mcelog_present" : }
+#$mcelog_present = mcelog_exists("/usr/sbin/mcelog")
+#  notify{ "Check File = $mcelog_present" : }
 #if $mcelog_exists == 1 {
 #  fail("Package is already absent. Unpin node from node group.")
 #}
 #if $::osfamily == 'Windows' {
 # #   fail("Operating system not supported")
 #}
-if $::mcelog_present == 1 and $::osfamily == 'RedHat' {
-  fail("Package is already absent. Unpin node from node group.")
-}
-elsif $::mcelog_present == 0 and $::osfamily == RedHat {
+#if $::mcelog_present == 1 and $::osfamily == 'RedHat' {
+#  fail("Package is already absent. Unpin node from node group.")
+#}
+if $::osfamily == RedHat {
 package { 'mcelog':
   ensure => 'absent',
 }
